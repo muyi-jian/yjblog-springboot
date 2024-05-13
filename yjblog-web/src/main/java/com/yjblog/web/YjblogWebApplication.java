34d4a36ecd0f189517409cfb896a8d89643a9c63
muyi-jian <2628168756@qq.com>
2024-05-13 21:02:51 +0800

    项目框架初始化

---
+++ yjblog-web/src/main/java/com/yjblog/web/YjblogWebApplication.java
@@ -0,0 +1,13 @@
+package com.yjblog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjblogWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YjblogWebApplication.class, args);
+    }
+
+}
