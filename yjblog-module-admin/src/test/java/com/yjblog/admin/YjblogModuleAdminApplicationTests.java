d0cb9a3ba8a8044739c5e172b7d5a21b5f020307
muyi-jian <2628168756@qq.com>
2024-05-14 22:27:40 +0800

    后台多模块搭建

---
+++ yjblog-module-admin/src/test/java/com/yjblog/admin/YjblogModuleAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjblog.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YjblogModuleAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ yjblog-module-common/src/test/java/com/yjblog/common/YjblogModuleCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjblog.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YjblogModuleCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
