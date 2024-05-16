dca882942e03cb1a52ff0425617ccfa27a8d9e62
muyi-jian <2628168756@qq.com>
2024-05-16 22:37:17 +0800

    自定义响应工具类和全局异常处理器

---
+++ yjblog-module-common/src/main/java/com/yjblog/common/exception/BaseExceptionInterface.java
@@ -0,0 +1,12 @@
+package com.yjblog.common.exception;
+
+/**
+ * @author 木白
+ * @date 2024/5/16
+ * @description 通用异常接口
+ */
+public interface BaseExceptionInterface {
+    String getErrorCode();
+
+    String getErrorMessage();
+}
--- yjblog-web/src/main/java/com/yjblog/web/controller/TestController.java
+++ yjblog-web/src/main/java/com/yjblog/web/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.yjblog.web.controller;
 
 import com.yjblog.common.aspect.ApiOperationLog;
+import com.yjblog.common.utils.Response;
 import com.yjblog.web.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +20,16 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
+    public Response  test(@RequestBody  @Validated User user ){
+        // 是否存在校验错误
+
+        // 手动抛异常，入参是前面定义好的异常码枚举，返参统一交给全局异常处理器搞定
+        // throw new BizException(ResponseCodeEnum.PRODUCT_NOT_FOUND);
         // 返参
-        return user;
+        // return Response.success();
+        // 主动定义一个运行时异常，分母不能为零
+
+        return Response.success();
     }
 
 }
